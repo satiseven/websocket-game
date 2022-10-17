0c50015d450fb238d72eed1107b4f70ff7a188ad
satiseven <satiseven777@gmail.com>
2022-10-18 00:32:36 +0300

    socket is ready

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-const app = require("expressjs");
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const port = process.env.PORT || 3000;
+app.use(express.static("public"));
+io.on("connect", (socket) => {
+  console.log("an user connected");
+  socket.on("disconnect", () => {
+    console.log("an user discounected");
+  });
+});
+server.listen(port, () => {
+  console.log("server is listen to port " + port);
+});
